247c6870ee33071d3e0692603d436472192dfd6e
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-08-19 18:37:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
-
-
 import streamlit as st
 import pandas as pd
-#import yfinance as yf
+import yfinance as yf
 from ta.volatility import BollingerBands
 from ta.trend import MACD, EMAIndicator, SMAIndicator
 from ta.momentum import RSIIndicator
