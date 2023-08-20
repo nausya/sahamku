787fcf3eac1ec85b46cab8c1f7c13903dc129e69
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-08-20 22:29:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ emiten = dataemiten['Kode'].sort_values(by='Kode')
 selected_emiten = st.sidebar.selectbox('Pilih Emiten:', emiten)
  
 ## Filter the data
-filtered_data = dataemiten.[dataemiten['Kode'] == selected_emiten]
+filtered_data = dataemiten.loc[dataemiten['Kode'] == selected_emiten]
  
 ## Display the filtered data
 #st.write(filtered_data)
