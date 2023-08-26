6feee941f9673a0a2abaf4012ae1976a1077c372
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-08-26 11:33:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ H52 = detil.info['fiftyTwoWeekHigh']
 C = detil.info['currentPrice']
 D = (H52-L52)/100
 P = (C - L52)/D
-st.subheader('Persentil =','round(P)')
+st.subheader('Persentil =','P')
 
 
 #Proses sidebar data
