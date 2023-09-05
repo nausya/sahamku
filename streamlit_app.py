2fd4ac9bda398a4e88028fc98ef767d0aeec2b1d
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-05 18:27:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ st.subheader(f"Harga terkini Rp{format(int(C),',d')}.- berada pada level {int(P)
 om  = detil.info['operatingMargins']
 dev = detil.info['payoutRatio']
 roe = detil.info['returnOnEquity']
-st.subheader(f"Op.Margin : {ceknon(om)}%, DevPay : {ceknon(dev)}%, ROE : {ceknon(roe)}%", divider="rainbow")
+st.subheader(f"MarginOps : {ceknon(om)}%, DevPR : {ceknon(dev)}%, ROE : {ceknon(roe)}%", divider="rainbow")
 st.info('Untuk jangka panjang perlu diperhatikan kisaran level harga kurang dari 10')
 
 
