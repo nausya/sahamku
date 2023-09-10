e2eb6cf7908b263ae8a0e9482371d3e7049f6b09
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-10 08:33:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -232,7 +232,7 @@ def screener():
         label = f"{c} {b}%-Dev:{d}%"
         plt.annotate(label,(a,b),textcoords="offset points",xytext=(4,-2), ha='left')
     
-    scr1 = scr1.rename(columns = {"kode": "Emiten", "skg": "Harga", "p": "Level", "om": "Margin Operasi", "dev": "DPR", "roe": "ROE"}).sort_values(['Level','Harga'])
+    scr1 = scr1.rename(columns = {"kode": "Emiten", "skg": "Harga", "p": "Level", "om": "Margin Operasi", "dev": "DPR", "roe": "ROE"}).sort_values(['Harga','Level'])
     scr1
 
 if __name__ == '__main__':
