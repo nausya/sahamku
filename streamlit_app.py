c8ef0b6ac447a06b8c545cc2ae6fc375ad159d9d
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-10 21:15:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -212,8 +212,8 @@ def screener():
     stocks = pd.read_csv('50p.csv')
     #stocks = stocks.to_string(index=False)
     #stocks['p'] = (stocks['skg'] - stocks['lo'])/((stocks['hi'] - stocks['lo'])/100)
-    scr1 = stocks.copy()
-    scr1 = scr1[scr1['kode','skg','p','om','dev','roe']]
+    #scr1 = stocks.copy()
+    scr1 = pd.DataFrame(stocks, columns = ['kode','skg','p','om','dev','roe'])
     scr1['p'] = round(scr1['p'],0)
     scr1['om'] = round(scr1['om'],2)*100
     scr1['dev'] = round(scr1['dev'],2)*100
