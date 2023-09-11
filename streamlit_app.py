d3e2473a860ad4e355a8b59f2bc164f4bb988322
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-11 21:42:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -221,10 +221,10 @@ def screener():
     #stocks['p'] = (stocks['skg'] - stocks['lo'])/((stocks['hi'] - stocks['lo'])/100)
     #scr1 = stocks.copy()
     scr1 = pd.DataFrame(stocks, columns = ['kode','skg','p','om','dev','roe'])
-    scr1['p'] = round((scr1['p'],0)
-    scr1['om'] = round(scr1['om'],2)*100
-    scr1['dev'] = round(scr1['dev'],2)*100
-    scr1['roe'] = round(scr1['roe'],2)*100
+    #scr1['p'] = round((scr1['p'],0)
+    #scr1['om'] = round(scr1['om'],2)*100
+    #scr1['dev'] = round(scr1['dev'],2)*100
+    #scr1['roe'] = round(scr1['roe'],2)*100
     #scr1= scr1.loc[(scr1['p']<11) & (scr1['skg']>50) & (scr1['om']>5) & (scr1['dev']>5) & (scr1['roe']>5)]
     
     
