fe21b563ae61d75866ffcba0b63bd9a39e40df5f
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-17 21:08:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -232,7 +232,7 @@ def screener():
     scr1['OpMargin'] = (scr1['OpMargin']*100).map('{:,.0f}%'.format)
     scr1['RoE'] = (scr1['RoE']*100).map('{:,.0f}%'.format)
     scr1 = scr1.rename(columns = {"Emiten":"Kode","Current": "Harga", "P": "Level", "OpMargin": "Margin Operasi", "DevPR": "DevPR", "RoE": "ROE"}).sort_values(['Harga','Level'])
-    scr1.style.set_properties(**{'text-align': 'right'})
+    pd.style.set_properties(**{'text-align': 'right'})
     scr1
     
 if __name__ == '__main__':
