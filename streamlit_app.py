c517d8765bdfa60009ec1607ede2001375e2670d
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-23 12:33:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,9 +256,11 @@ def screener():
     scr1['DevPR'] = (scr1['DevPR']*100).map('{:,.0f}%'.format)
     scr1['OpMargin'] = (scr1['OpMargin']*100).map('{:,.0f}%'.format)
     scr1['RoE'] = (scr1['RoE']*100).map('{:,.0f}%'.format)
+    skatersaham = scr1
     scr1 = scr1.rename(columns = {"Emiten":"Kode","Current": "Harga", "P": "Level", "OpMargin": "Margin Operasi", "DevPR": "DevPR", "RoE": "ROE"}).sort_values(['Harga','Level'])
     scr1
-
+    skater (skatersaham)
+    
     #scr2 = pd.read_csv('PersentilN.csv', usecols=["Kode","Current","P","Industri","Marcap(M)","VolAvg","Vol","Share(Juta)","CHG%"],index_col=[0])
     #sektor = scr2['Industri'].unique()
     #screensektor = st.selectbox('Pilih Sektor:', sektor)
