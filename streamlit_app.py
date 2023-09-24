2c1033142cddf10864613025944f801030ff1766
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-24 19:51:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,10 @@ H52 = df['hi']
 C = df['skg']
 D = (H52-L52)/100
 st.write(f"Nilai D:{D}")
-P = (C - L52)/D
+if L52=H52:
+ P = 0
+else:
+ P = (C - L52)/D
 st.write(f"Nilai P:{P}")
 st.subheader(f"Harga terkini Rp{int(C)}.- berada pada level {int(P)} dari skala 100", divider="rainbow")
 om  = df['om']
