e5e1675b882a03ccfddab25c0fec4bf26d0cb4c6
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-26 21:30:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ def notasi(kode):
     return st.text("")
    else:  
     n = n['Keterangan Notasi'].values[0]
-    return st.write(n)
+    return st.error('''n''')
 
 #Display Persentil
 saham = [option]
