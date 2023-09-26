34a94871dd906bdf713ff2c13f0d29f8a0a323cc
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-26 21:24:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,7 +115,7 @@ roe = df['roe']
 
 st.subheader(f"MarginOps : {ceknon(om)}%, DevPR : {ceknon(dev)}%, ROE : {ceknon(roe)}%", divider="rainbow")
 kode = selected_emiten.split(' | ')[0]
-print(notasi(kode))
+st.error(notasi(kode))
 
 #st.markdown('notasi(kode)') # see *
 st.info('Untuk jangka panjang perlu diperhatikan kisaran level harga kurang dari 10')
