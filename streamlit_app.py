d3c3034b8e6348f97b56443e10106386579f9066
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-30 23:02:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -270,12 +270,13 @@ def screener():
     fig, ax = plt.subplots()
     x = s['P']
     y = s['OpMargin']*100
+    kd = s['Kode']
     z = (s['DevPR']*100).map('{:,.0f}%'.format)
     #st.write ([x,y,z])
     sns.scatterplot(x=x, y=y, marker='>')
     plt.xlabel("Level Harga")
     plt.ylabel("Margin Operasi (%)")
-    for a,b,c,d in zip(x,y,kd,dev):
+    for a,b,c,d in zip(x,y,kd,z):
         label = f"{c} {b}%-Dev:{d}%"
         ax.annotate(label,(a,b), xytext=(3, -3),textcoords='offset points')
     st.pyplot(fig)
