282634fd345029509694b6db1375b9637fae1d02
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-09-30 22:16:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -271,8 +271,8 @@ def screener():
     x = s['P']
     y = s['OpMargin']*100
     z = (s['DevPR']*100).map('{:,.0f}%'.format)
-    st.write (z)
-    sns.scatterplot(s, x=x, y=y, z=z, ax=ax, marker='>')
+    st.write (z[0])
+    sns.scatterplot(s, x=x, y=y, ax=ax, marker='>')
     plt.xlabel("Level Harga")
     plt.ylabel("Margin Operasi (%)")
     for xi, yi, text in zip(x, y, z):
