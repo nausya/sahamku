37d0cba612ce8ef49d07be9303f6f8367b6851d3
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2023-10-15 00:31:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -269,6 +269,8 @@ def screener():
     y = s['OpMargin']*100
     kd = s['Kode']
     #st.write (x)
+    sns.set( rc = {'figure.figsize' : ( 2, 2 ),  
+               'axes.labelsize' : 12 }) 
     sns.scatterplot(s,x=x, y=y, marker='>')
     plt.rcParams['figure.figsize'] = [2, 2]
     plt.xlabel("Level Harga")
