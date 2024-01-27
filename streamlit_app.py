039eaa308073f64efb9d17e7ab8aa5a5e7694c81
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-01-27 22:12:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,9 +257,9 @@ def screener():
     s = scr1.copy()
     #scr1['Current'] = scr1['Current'].astype(int)
     #scr1['P'] = scr1['P'].astype(int)+ "%"
-    scr1['DevPR'] = (scr1['DevPR'].astype(int)+ "%"
-    scr1['OpMargin'] = (scr1['OpMargin'].astype(int)+ "%"
-    scr1['RoE'] = (scr1['RoE'].astype(int)+ "%"
+    scr1['DevPR'] = scr1['DevPR'].astype(int)+ "%"
+    scr1['OpMargin'] = scr1['OpMargin'].astype(int)+ "%"
+    scr1['RoE'] = scr1['RoE'].astype(int)+ "%"
    
     scr1 = scr1.rename(columns = {"Emiten":"Kode","Current": "Harga", "P": "Level", "OpMargin": "Margin Operasi", "DevPR": "DevPR", "RoE": "ROE"}).sort_values(['Harga','Level'])
     st.dataframe(scr1.style.highlight_max(axis=0))
