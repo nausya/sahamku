fe8395f69a5bdbf94c70ccd611c5c4769419c391
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-02-24 20:41:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -194,6 +194,7 @@ def dataframe():
         indsektor = pd.read_csv('IndexSektor.csv', index_col=[0], sep=';')
         st.dataframe(indsektor)
     elif caridata == 'Finansial':
+        st.slider("Isikan Dengan Angka", 0, 100)
         st.header("Finansial (Milyar Rupiah)")
         keu = pd.read_csv('Finansial.csv', index_col=[0], sep=';')
         st.dataframe(keu)
