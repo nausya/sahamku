5d62819920eb5d6aa15f89162bdf483e1889807a
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-02 22:12:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ fin = fin.query("Kode==@kodef")
 fin = fin[['EPSRP','BVRP','PER','PBV','Sektor','KodeInd']]
 if fin.empty:
    print ("Kategori Perusahaan Baru IPO")
-else
+else:
    fin = fin.values.tolist()
    fin = [item for sublist in fin for item in sublist]
    eps = (fin[0])
