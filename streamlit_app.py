06f681c66337745dba21dd4ca385f9cfe5a09b20
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-02 22:13:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,21 +123,19 @@ else:
    ind = fin[5][:2]
    st.subheader(f"EPS : Rp.{eps} | BV : Rp.{bv} | PBV : {pbv} | PER : {per} Sektor : {sek}", divider="rainbow")
 
-#BENCHMARK
-kodebm = ind
-bm = pd.read_csv('IndexSektor.csv', sep=";")
-bm = bm.query("Kode==@kodebm")
-
-
-bm = bm[['Sektor','EPSRp','BVRp','PER','PBV']]
-bm = bm.values.tolist()
-bm = [item for sublist in bm for item in sublist]
-bmeps = bm[1]
-bmbv = bm[2]
-bmpbv = bm[4]
-bmper = bm[3]
-bmsek = bm[0]
-st.subheader(f"EPS : Rp.{bmeps} | BV : Rp.{bmbv} | PBV : {bmpbv} | PER : {bmper} | SubSektor : {bmsek}", divider="rainbow")
+   #BENCHMARK
+   kodebm = ind
+   bm = pd.read_csv('IndexSektor.csv', sep=";")
+   bm = bm.query("Kode==@kodebm")
+   bm = bm[['Sektor','EPSRp','BVRp','PER','PBV']]
+   bm = bm.values.tolist()
+   bm = [item for sublist in bm for item in sublist]
+   bmeps = bm[1]
+   bmbv = bm[2]
+   bmpbv = bm[4]
+   bmper = bm[3]
+   bmsek = bm[0]
+   st.subheader(f"EPS : Rp.{bmeps} | BV : Rp.{bmbv} | PBV : {bmpbv} | PER : {bmper} | SubSektor : {bmsek}", divider="rainbow")
 
 ##########Notasi Saham################
 kode = selected_emiten.split(' | ')[0]
