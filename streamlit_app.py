d885fcf1c7610166a9b0ab1550210dfce7451840
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-02 18:54:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ kodef = selected_emiten.split(' | ')[0]
 f = pd.read_csv('Finansial.csv', sep=";")
 f = f.query("Kode==@kodef")
 f = f[['EPSRP','BVRP','PER','PBV']]
-f = f.astype(float)
+#f = f.astype(float)
 f = f.fillna(0)
 f.dtypes
 eps = f['EPSRP']
