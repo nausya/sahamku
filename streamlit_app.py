5bb55d100c1644358d3bd7d6452de66102166a45
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-03 21:58:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,7 +129,7 @@ else:
    sek = fin[4]
    ind = fin[5][:2]
     
-   st.subheader(f"EPS : Rp.{eps} | BV : Rp.{bv} | PBV : {pbv} | PER : {per} | Sektor : {sek}", divider="rainbow")
+   st.subheader(f"EPS : Rp.{round(eps,2)*100} | BV : Rp.{bv} | PBV : {pbv} | PER : {per} | Sektor : {sek}", divider="rainbow")
 
    #BENCHMARK
    kodebm = ind
