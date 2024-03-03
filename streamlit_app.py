bb5b201c1f868ea11c1f11d2bd2c7b1d7c11a503
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-03 11:39:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,7 @@ else:
    P = ['5']#P.values[0]
    P
    dfringkas = []
-   dfringkas.append({'kode':kode,'skg':skg,'lo':lo,'hi':hi,'om':om,'dev':dev,'roe':roe,'pos':P,'eps':eps,'bv':bv,'pbv':pbv,'per':per})
+   dfringkas.append({'kode':kode,'skg':skg,'lo':lo,'hi':hi,'om':om,'dev':dev,'roe':roe,'pos':P[0],'eps':eps,'bv':bv,'pbv':pbv,'per':per})
    dfringkas = pd.DataFrame(screensaham)
    dfringkas = dfringkas.fillna(0)
    st.dataframe(dfringkas)
