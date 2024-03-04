58ce1744ccb162ab64585bfdb6b5640d71b4ca8f
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-04 22:04:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,7 +96,7 @@ for stock in saham:
     om  = ceknon(info.get('operatingMargins'))
     dev = ceknon(info.get('payoutRatio'))
     roe =  ceknon(info.get('returnOnEquity'))
-    pery = float(info.get('forwardPE'))
+    pery = ceknon(info.get('forwardPE'))
     epsy = info.get('forwardEps')
     pbvy = info.get('priceToBook')
     bvy = info.get('bookValue')
