47c81648723ccf0d465b65cf770a29e9eab303ae
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-06 12:51:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,6 +115,7 @@ for stock in saham:
                        'm':m,'cash':cash,'opcash':opcash,'ph':ph,'ut':ut,'tcs':tcs})
 df = pd.DataFrame(screensaham)
 df = df.fillna(0)
+df = df.loc[:, df.columns != 'kode'].astype(float)
 L52 = df['lo']
 H52 = df['hi']
 C = df['skg']
