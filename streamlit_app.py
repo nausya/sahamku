fb6f49e6cf4ce66dd2486ac602057872e132e463
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-06 21:46:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,8 +186,8 @@ else:
    opcash = opcash/1000000000
    vol = vol/1000000
 
-   dfringkas = {'kode':kode,'date':date,'skg':skg,'1ylo':lo,'6m':int(m),'1yhi':hi,'om(%)':om,
-                'dev(%)':dev,'roe(%)':roe,'pos':round(P,0),'cash(M)':round(cash),'opcash(M)':int(opcash),'ph(M)':int(ph),
+   dfringkas = {'kode':kode,'date':date,'skg':skg,'1ylo':lo,'6m':int(m),'1yhi':hi,'om(%)':ceknon(om),
+                'dev(%)':ceknon(dev),'roe(%)':ceknon(roe),'pos':round(P,0),'cash(M)':round(cash),'opcash(M)':int(opcash),'ph(M)':int(ph),
                 'utang(M)':ut,'cash/saham(Rp)':tcs,'eps(Rp)':eps,'bv(Rp)':bv,'pbv':pbv,
                 'per':per,'vol(Juta)':round(vol,0),'aksiy':aksiy,'aksik':aksik,'user':id}
    
