da5fb2cbffe5db609c40aea0beb5ac9596559341
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-07 14:19:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -202,7 +202,7 @@ else:
    dfringkas = {'kode':kode,'date':date,'skg':skg,'1ylo':lo,'6m':int(m),'1yhi':hi,'om(%)':ceknon(om),
                 'dev(%)':ceknon(dev),'roe(%)':ceknon(roe),'pos':round(P,0),'cash(M)':round(cash),'opcash(M)':int(opcash),'ph(M)':int(ph),
                 'utang(M)':round(ut),'cash/saham(Rp)':round(tcs),'eps(Rp)':eps,'bv(Rp)':round(bv),'pbv':round(pbv,1),
-                'per':round(per),'vol(Juta)':round(vol,0),'Omzet(M)':round(mcap:1.f),'aksiy':aksiy,'aksik':aksik,'user':id}
+                'per':round(per),'vol(Juta)':round(vol,0),'Omzet(M)':round(mcap),'aksiy':aksiy,'aksik':aksik,'user':id}
    
   # with open('aksi.csv', 'a') as f_object:
    #     writer_object = writer(f_object)
