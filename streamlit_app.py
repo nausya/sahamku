b62a7ae181ad70d5771aa8c89cad1144417c6560
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-09 01:23:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -307,8 +307,8 @@ def dataframe():
        st.header('Filter Data')
        filterdata = pd.read_csv('porto.csv', index_col=[0], sep=';')
        tanggal = filterdata['date'].values[0]
-       #tanggal = tanggal.strftime('%d/%m/%Y')
-       type(tanggal)
+       tanggal = tanggal.dt.date
+       
        "Last Update : " + tanggal
        filterdata = filterdata.rename(columns = {"p": "Posisi","kode":"Kode","aksiy": "Saran","skg":"Harga","lo":"1YMin","hi":"1YMax","bl":"2M","m":"6M", 
        "opm":"Margin Operasi(%)", "dev":"Deviden PR(%)","epsy":"Laba Per Saham","roe": "ROE(%)","pery": "PER(%)",
