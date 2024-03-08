9d00387be5a1f79fd6d31cbce87c716d24e05ab6
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-09 00:53:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -303,7 +303,7 @@ def dataframe():
     else:
        st.header('Filter Data')
        filterdata = pd.read_csv('porto.csv', index_col=[0], sep=';')
-       filterdata[date].unique().values[0]
+       filterdata[date].unique()
        filterdata = filterdata.rename(columns = {"p": "Posisi","kode":"Kode","rekom": "Saran","now":"Harga","l":"1YMin","h":"1YMax","bl":"2M","m":"6M", 
        "opm":"Margin Operasi(%)", "dev":"Deviden PR(%)","epsy":"Laba Per Saham","roe": "ROE(%)","pery": "PER(%)",
        "pbvy": "Nilai Buku","bvy": "Harga Dasar","dte": "Rasio UM","eg4": "PhGrow","ph": "Pendapatan","ut":"Utang",
