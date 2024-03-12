7e4f210f251365b35de98d7eb5950fc7a40a8e64
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-12 21:09:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,7 +186,9 @@ else:
    ddev = -100
    dom = -3
    dnpm = -19
-   dtcs = dpros(skg,tcs)
+   C
+   tcs
+   dtcs = dpros(C,tcs)
    droe = 44
    # metric1 
    col1, col2, col3, col4, col5 = st.columns(5)
