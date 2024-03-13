e6c134997c41ab7b4665cce107f4a1bd0dace51a
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-13 18:05:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,7 +196,7 @@ else:
    bm = bm[['Sektor','EPSRp','BVRp','PER','PBV','DER']]
    bm = bm.values.tolist()
    bm = [item for sublist in bm for item in sublist]
-   bmeps = bm[1]
+   bmeps = round(bm[1])
    bmbv = bm[2]
    bmpbv = bm[4]
    bmper = bm[3]
