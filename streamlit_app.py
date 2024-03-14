b171e214b578cdbf21e0d9680716122916b15b5e
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-15 04:39:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -240,6 +240,9 @@ else:
    dbv = round(d(bv, bmbv))
    dpbv = round(d(bmpbv, pbv))
    dper = round(d(bmper, per))
+   Volrata = data.tail(1)
+   Volrata = Volrata['Volume'].mean()
+   Volrata
    dvol = round(d(vol, vole))
    ddev = 0
    dom = 0
