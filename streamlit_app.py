0e44398331eb6e094783d20f0188d18320c9e966
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-15 21:51:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -258,73 +258,9 @@ fig1 = go.Figure(
         ]
     )
 )
-#fig1
+fig1
 #####END OF CHART
-######CHART
-plot_bgcolor = "#fff"
-quadrant_colors = [plot_bgcolor, "red", "#f2a529", "#eff229", "#85e043", "#2bad4e"] 
-quadrant_text = ["", "<b>Kuat Jual</b>", "<b>Jual</b>", "<b>Netral</b>", "<b>Beli</b>", "<b>Kuat Beli</b>"]
-n_quadrants = len(quadrant_colors) - 1
 
-current_value = P
-min_value = 0
-max_value = 100
-hand_length = np.sqrt(2) / 4
-hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_value) / (max_value - min_value))
-
-fig2 = go.Figure(
-    data=[
-        go.Pie(
-            values=[0.5] + (np.ones(n_quadrants) / 2 / n_quadrants).tolist(),
-            rotation=90,
-            hole=0.5,
-            marker_colors=quadrant_colors,
-            text=quadrant_text,
-            textinfo="text",
-            hoverinfo="skip",
-        ),
-    ],
-    layout=go.Layout(
-        showlegend=False,
-        margin=dict(b=0,t=10,l=10,r=10),
-        width=275,
-        height=275,
-        paper_bgcolor=plot_bgcolor,
-        annotations=[
-            go.layout.Annotation(
-                text=f"<b>Posisi : </b>{current_value}",
-                x=0.5, xanchor="center", xref="paper",
-                y=0.6, yanchor="bottom", yref="paper",
-                showarrow=False,
-            ),
-            go.layout.Annotation(
-                text=f"<b>Jangka Panjang</b>",
-                x=0.5, xanchor="center", xref="paper",
-                y=0.4, yanchor="bottom", yref="paper",
-                showarrow=False)
-        ],
-        shapes=[
-            go.layout.Shape(
-                type="circle",
-                x0=0.48, x1=0.52,
-                y0=0.48, y1=0.52,
-                fillcolor="#333",
-                line_color="#333",
-            ),
-            go.layout.Shape(
-                type="line",
-                x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
-                y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
-                line=dict(color="#333", width=4)
-            )
-        ]
-    )
-)
-#fig2
-#####END OF CHART
-col1, col2 = st.columns(2)
-col1.metric("",fig1,"")
-col2.metric("",fig2,"")
 #FINANSIAL
 kodef = selected_emiten.split(' | ')[0]
 fin = pd.read_csv('Finansial.csv', sep=";")
