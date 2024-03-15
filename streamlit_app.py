ecd560699980af88c678865288906e2b7a9729f1
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-15 17:45:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import seaborn.objects as so
 import yfinance as yf
 import numpy as np
 import plotly.figure_factory as ff
+import plotly.graph_objects as go
 from matplotlib import markers
 from streamlit_option_menu import option_menu
 from ta.volatility import BollingerBands
@@ -25,6 +26,41 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
 
+
+
+        fig.add_trace(go.Indicator(
+            mode="gauge+number+delta",
+            title = {'text': "price"},
+            delta = {'reference': ask_price, 'relative': False, 'increasing': {'color': "RebeccaPurple"}, 'decreasing': {'color': "RoyalBlue"}},
+            value = current_price,
+            domain={'x': [0, 1], 'y': [0, 1]},
+            gauge={
+                'shape': 'angular',
+                'axis': {'range': [bid_price - spread, ask_price + spread]},
+                #'axis': {'range': [lower_limit, upper_limit]},
+                'bar': {'color': "darkblue"},
+                'bgcolor': 'yellow',
+                'borderwidth': 2,
+                'bordercolor': 'black',
+                'steps': [
+                    {'range': [bid_price*0.9 , bid_price], 'color': 'green'},
+                    {'range': [ask_price , ask_price*1.1], 'color': 'red'}
+                ],
+                'threshold': {
+                    'line': {'color': 'orange', 'width': 6},
+                    'thickness': 0.75,
+                    'value': current_price,
+                }
+            },
+            #number={'prefix': 'BTC'}
+        ))
+        
+        # Adjust the width and height of the gauge
+        #fig.update_layout(height=500, width=800)
+        with fig_placeholder:
+                #st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+                st.plotly_chart(fig, use_container_width=True)
+                #st.plotly_chart(fig)
 
 # Tentukan zona waktu Indonesia
 indonesia_timezone = pytz.timezone('Asia/Jakarta')
