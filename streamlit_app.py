e41db0322a7e6c6babf6d0be1a6742d4857a9189
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-15 17:49:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,38 +29,38 @@ from sklearn.metrics import r2_score, mean_absolute_error
 
 
 fig.add_trace(go.Indicator(
-            mode="gauge+number+delta",
-            title = {'text': "price"},
-            delta = {'reference': ask_price, 'relative': False, 'increasing': {'color': "RebeccaPurple"}, 'decreasing': {'color': "RoyalBlue"}},
-            value = current_price,
-            domain={'x': [0, 1], 'y': [0, 1]},
-            gauge={
-                'shape': 'angular',
-                'axis': {'range': [bid_price - spread, ask_price + spread]},
-                #'axis': {'range': [lower_limit, upper_limit]},
-                'bar': {'color': "darkblue"},
-                'bgcolor': 'yellow',
-                'borderwidth': 2,
-                'bordercolor': 'black',
-                'steps': [
-                    {'range': [bid_price*0.9 , bid_price], 'color': 'green'},
-                    {'range': [ask_price , ask_price*1.1], 'color': 'red'}
-                ],
-                'threshold': {
-                    'line': {'color': 'orange', 'width': 6},
-                    'thickness': 0.75,
-                    'value': current_price,
+mode="gauge+number+delta",
+title = {'text': "price"},
+delta = {'reference': ask_price, 'relative': False, 'increasing': {'color': "RebeccaPurple"}, 'decreasing': {'color': "RoyalBlue"}},
+value = current_price,
+domain={'x': [0, 1], 'y': [0, 1]},
+gauge={
+      'shape': 'angular',
+      'axis': {'range': [bid_price - spread, ask_price + spread]},
+      #'axis': {'range': [lower_limit, upper_limit]},
+     'bar': {'color': "darkblue"},
+     'bgcolor': 'yellow',
+     'borderwidth': 2,
+     'bordercolor': 'black',
+     'steps': [
+            {'range': [bid_price*0.9 , bid_price], 'color': 'green'},
+            {'range': [ask_price , ask_price*1.1], 'color': 'red'}
+            ],
+     'threshold': {  
+            'line': {'color': 'orange', 'width': 6},
+            'thickness': 0.75,
+            'value': current_price,
                 }
-            },
-            #number={'prefix': 'BTC'}
-        ))
+      },
+      #number={'prefix': 'BTC'}
+      ))
         
-        # Adjust the width and height of the gauge
-        #fig.update_layout(height=500, width=800)
-        with fig_placeholder:
-                #st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-                st.plotly_chart(fig, use_container_width=True)
-                #st.plotly_chart(fig)
+# Adjust the width and height of the gauge
+#fig.update_layout(height=500, width=800)
+with fig_placeholder:
+    #st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
+    #st.plotly_chart(fig)
 
 # Tentukan zona waktu Indonesia
 indonesia_timezone = pytz.timezone('Asia/Jakarta')
