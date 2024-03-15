e8cdff077641f2f1da113b8b98a02ce7df38b408
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-15 17:51:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,34 +27,26 @@ from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
 
 
+fig = go.Figure(go.Indicator(
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    value = 4.3,
+    mode = "gauge+number+delta",
+    title = {'text': "General satisfaction"},
+    delta = {'reference': 2.5},
+    gauge = {'axis': {'range': [None, 5], 'tickwidth': 1,'tickcolor': "black"},
+        'bar': {'color': "MidnightBlue"},
+             'steps' : [
+                 {'range': [0, 1], 'color': "DarkTurquoise"},
+                 {'range': [1, 2], 'color': "MediumTurquoise"},
+                 {'range': [2, 3], 'color': "Turquoise"},
+                 {'range': [3, 4], 'color': "PaleTurquoise"},
+                 {'range': [4, 5], 'color': "lightcyan"}],
+             'threshold' : {'line': {'color': "brown", 'width': 4}, 'thickness': 0.75, 'value': 4.8}}))
 
-fig.add_trace(go.Indicator(
-mode="gauge+number+delta",
-title = {'text': "price"},
-delta = {'reference': ask_price, 'relative': False, 'increasing': {'color': "RebeccaPurple"}, 'decreasing': {'color': "RoyalBlue"}},
-value = current_price,
-domain={'x': [0, 1], 'y': [0, 1]},
-gauge={
-      'shape': 'angular',
-      'axis': {'range': [bid_price - spread, ask_price + spread]},
-      #'axis': {'range': [lower_limit, upper_limit]},
-     'bar': {'color': "darkblue"},
-     'bgcolor': 'yellow',
-     'borderwidth': 2,
-     'bordercolor': 'black',
-     'steps': [
-            {'range': [bid_price*0.9 , bid_price], 'color': 'green'},
-            {'range': [ask_price , ask_price*1.1], 'color': 'red'}
-            ],
-     'threshold': {  
-            'line': {'color': 'orange', 'width': 6},
-            'thickness': 0.75,
-            'value': current_price,
-                }
-      },
-      #number={'prefix': 'BTC'}
-      ))
-        
+
+fig.show()
+
+
 # Adjust the width and height of the gauge
 #fig.update_layout(height=500, width=800)
 with fig_placeholder:
