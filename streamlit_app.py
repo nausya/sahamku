90d55b777fafdc9471b36cbe30e4a04f0a7b88c1
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-16 07:13:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -228,8 +228,7 @@ fig1 = go.Figure(
                 x=0.5, xanchor="center", xref="paper",
                 y=0.6, yanchor="bottom", yref="paper",
                 showarrow=False,
-                font=dict(
-                    size=20  # Change font size here
+                font=dict(size=20) # Ubah ukuran font di sini
             ),
             go.layout.Annotation(
                 text=f"<b>Jangka Pendek <br>(Gorengan)</b>",
