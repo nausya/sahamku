7ba9a6f37c10f69cba69d5f0e7cbfac3a1dcc050
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-16 10:16:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -217,9 +217,10 @@ fig1 = go.Figure(
         ),
     ],
     layout=go.Layout(
+        title = "Posisi Harga 2 Bulan Terakhir",
         showlegend=False,
         margin=dict(b=0,t=10,l=10,r=10),
-        autosize= True,#width=275,
+        width=275,
         height=275,
         paper_bgcolor=plot_bgcolor,
         annotations=[
