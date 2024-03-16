33146db5a38217def711111062c2ed8ad4d0963f
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-16 07:14:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -224,7 +224,7 @@ fig1 = go.Figure(
         paper_bgcolor=plot_bgcolor,
         annotations=[
             go.layout.Annotation(
-                text=f"<b>Posisi : </b>{current_value}",
+                text=f"Posisi : <br><b>{current_value}</b>",
                 x=0.5, xanchor="center", xref="paper",
                 y=0.6, yanchor="bottom", yref="paper",
                 showarrow=False,
