c18add05ccdb8afd383eb08cb8301a06e40e222b
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-18 04:57:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,7 +172,6 @@ H52 = df['hi'].values[0]
 C = df['skg'].values[0]
 ##### BILA DATA LIVE BELUM TERSEDIA skg;lo;bl;m;hi
 HgPor = pd.read_csv('porto.csv', index_col=[0], sep=';')
-kodek
 option
 kodesaja
 HgPor = HgPor.query("kode =='GOTO'")
