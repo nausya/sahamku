c2cdcea8f9e8d91baa3d1acca855450e9e4c01f9
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-17 17:08:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -418,8 +418,8 @@ else:
 
    dfringkas = pd.DataFrame(dfringkas, index = np.arange(1))
    dfringkas = dfringkas.set_index('Kode')
-   dfringkas['Tanggal'] = pd.to_datetime(dfringkas['Tanggal'], format='%Y-%m-%d %H:%M:%S')
-   dfringkas['Tanggal']
+   dfringkas['Tanggal'] = pd.to_datetime(dfringkas['Tanggal'], format='%Y-%m-%d')
+   #dfringkas['Tanggal']
    st.dataframe(dfringkas)
 
 st.info('Untuk jangka panjang perlu diperhatikan kisaran posisi harga kurang dari 10')
