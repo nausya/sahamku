1d7fecc8dda09f12bd328696524ffd083647c75c
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-18 20:33:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -231,8 +231,8 @@ with col1:
            st.error('Jumlah Minimal Beli 1 Lot Saham')
     with tab3:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
         #dfjual = pd.read_csv('aksi.csv', sep=";", use_cols=['date','kode','skg'])
-        col1 = st.columns([1, 1])
-        col1.metric("Rp800Rb", 10 + 'Lot', -6 + '%')
+        col1 = st.columns(1)
+        col1.metric("Rp800Rb", 10, -6)
         #st.write('Simulasi Jual')                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 
 ######End of Halaman Utama
