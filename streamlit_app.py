6fd03fd338882481e1289c65e88215b8220c32c6
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-18 21:28:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -236,7 +236,7 @@ with col1:
         vj = dfjual['vol'].values[0]
         tj = dfjual['total'].values[0]
         mj = digit(round(tj - (C * vj)))
-        st.metric(f"Margin : Rp{mj}", vj/100, "mj")
+        st.metric(f"Margin : -6%", vj/100, "{mj}")
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 
