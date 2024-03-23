c2cf35deccdc9768657ac5c0acee0e90c5ccc89c
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-24 05:19:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -411,7 +411,7 @@ else:
    utlap = fin[6] * 1000000000
    sek = fin[4]
    ind = fin[5][:2]
-   prf = fin[6]
+   prf = fin[7] * 1000
    per = per if per !='Infinity' else 0
    vol = int(vol) if vol !=None else 0
    vole = int(vole) if vole !=None else 0
@@ -459,7 +459,7 @@ else:
    col6.metric("Deviden", str(ceknon(dev)) + "%", 0)
    col7.metric("Kas Operasional", digit(opcash), dom)
    col8.metric("Uang Tunai(Rp)", tunai, utun)
-   col9.metric("Untung", round(prf), 0)
+   col9.metric("Untung", digit(round(prf)), 0)
    col10.metric("Utang", digit(ut), digit(utlap))
    st.subheader("", divider="rainbow")
 
