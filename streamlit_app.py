9e67c992973004c9b59d50ab2e52a08522f4cc37
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-30 05:55:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,7 +257,7 @@ with col1:
             prosj = dpros(tj,tb) - 100
             mj = tj - tb
             lj = round(vj/100)
-            st.metric(f"Beli di harga Rp{round(hb)}. \nMargin Simulasi Penjualan Saham {kodesaja} : {round(prosj,1)}%", f"{lj} Lot", digit(mj) + prosj) 
+            st.metric(f"Beli di harga Rp{round(hb)}. \nMargin Simulasi Penjualan Saham {kodesaja} : {round(prosj,1)}%", f"{lj} Lot", f"{digit(mj)} + {prosj}") 
 
     with tab4:
         mm = yf.Ticker(option).history(period="max")                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
