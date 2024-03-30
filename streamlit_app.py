e4ca5de4ad5707ade7be5d02f85e77ffa211bb09
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-03-30 22:06:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -562,7 +562,7 @@ def dataframe():
         with tab1:
            st.write('Filter Data')
            filterdata = pd.read_csv('porto.csv', index_col=[0], sep=';')
-           p = filterdata['p'].values[0]
+           filterdata['p'] = filterdata['p'].values[0]
            #p = round(p)
            tgl = filterdata['date'].values[0]
            tgl = tgl[8:10] + "/" + tgl[5:7]+ "/" + tgl[0:4]
