cf90b5c6207e7c15a85064bea70834890fbb7aa1
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-04-18 20:21:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -702,7 +702,11 @@ def screener():
     "Last Update : " + tgl
     ### ditambahkan 17 april 2024 utk notif notasi warna pink
     scr1 = scr1.style.applymap(lambda x: color_cell(x) if x in nota['Kode'].values else '')
-    st.dataframe(scr1)
+    # Render DataFrame sebagai HTML
+    html = scr1.render()
+    # Tampilkan HTML di Streamlit
+    st.write(html, unsafe_allow_html=True)
+    #st.dataframe(scr1)
     st.subheader('Grafik')
     fig, ax = plt.subplots()
     
