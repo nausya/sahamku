849ab378bdedc13a4dcf2bb7694147e0ddf07d7d
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-04-21 06:08:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -656,7 +656,7 @@ def screener():
     with col1:
         awal, akhir = st.slider('Posisi', min_value=0, max_value=100, value=(0, 100))
     with col2:
-        omawal, omakhir = st.slider('Margin Operasi', min_value=0%, max_value=100%, value=(0%, 100%))
+        omawal, omakhir = st.slider('Margin Operasi', min_value=0, max_value=100, value=(0, 100))
     if screenlevel == '<Rp200':
        st.write('Screener Saham Harga Rentang 50-200')
        scr1=scr1.query("skg > 50 and skg<= 200 and p>=@awal and p<=@akhir and om >= 0.1 and roe >= 0.1")
