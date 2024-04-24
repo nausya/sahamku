887b870528bfa153a90c5831eb4f3002795e3d9f
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-04-24 22:56:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -648,7 +648,7 @@ def screener():
     #tgl = tgl[8:10] + "/" + tgl[5:7]+ "/" + tgl[0:4]
     
     scr1 = scr1.fillna(0)
-    scr1['tun'] = scr1['tcs']/scr1['skg']*100
+    scr1['tun'] = scr1['tcs']/scr1['skg']
     col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1], gap="large")
     with col1:
         awal, akhir = st.slider('Posisi', min_value=0, max_value=100, value=(0, 100))
@@ -699,7 +699,7 @@ def screener():
   
     else:
        st.subheader('Screener Saham Dengan Fraksi Harga Lebih Dari 5000')
-       scr1 = scr1.query("skg > 5000 and p>=@awal and p<=@akhir and om>=@omawal and om<=@omakhir and dev>=@devawal and dev<=@devakhir and roe>=@roeawal and roe<=@roeakhir")
+       scr1 = scr1.query("skg > 5000 and p>=@awal and p<=@akhir and om>=@omawal and om<=@omakhir and dev>=@devawal and dev<=@devakhir and roe>=@roeawal and roe<=@roeakhir and tun>=@tunawal and tun<=@tunakhir")
     scr2 = scr1.copy()
     scr2 = scr2.style.applymap(lambda x: 'background-color: pink' if x in nota['Kode'].values else '')
     s = scr1.copy()
