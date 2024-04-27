2f2be12ad460985cc09778eb455926390456f4dd
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-04-27 22:19:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -693,15 +693,6 @@ def screener():
         subsek = subsek['Kode']
         subsek = subsek.values.tolist()
         scr1 = scr1.query("kode in @subsek")
- 
-            #indus = ind['SubIndustri']
-            #indus = indus.drop_duplicates()
-            #pilihind = st.selectbox('Pilih Industri :', indus)
-            #subind = ind.query("SubIndustri == @pilihind")
-            #subind = subind['Kode']
-            #subind = subind.values.tolist()
-            #scr1 = scr1.query("kode in @subind")
-            #scr1 = scr1.query("kode in @subind and p>=@awal and p<=@akhir and om>=@omawal and om<=@omakhir and dev>=@devawal and dev<=@devakhir and roe>=@roeawal and roe<=@roeakhir and tun>=@tunawal and tun<=@tunakhir and pbvy>=@nbawal and pbvy<=@nbakhir")
     
     else:
         st.subheader('Screener Saham Dengan Fraksi Harga Lebih Dari 5000')
