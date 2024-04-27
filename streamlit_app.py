1d449122420c4c91f0c6d495488a99215bf8d0a8
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-04-27 10:43:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -705,14 +705,11 @@ def screener():
         indus = ind['SubIndustri']
         indus = indus.drop_duplicates()
         pilihind = st.selectbox('Pilih Industri :', indus)
-        pilihind
         subind = ind.query("SubIndustri == @pilihind")
         subind = subind['Kode']
         subind = subind.values.tolist()
         scr1 = scr1.query("kode in @subind")
-        scr1 
-        #scr1 = scr1.query("kode in @subind and p>=@awal and p<=@akhir and om>=@omawal and om<=@omakhir and dev>=@devawal and dev<=@devakhir and roe>=@roeawal and roe<=@roeakhir and tun>=@tunawal and tun<=@tunakhir and pbvy>=@nbawal and pbvy<=@nbakhir")
-        #st.write('Industri')#(f'{pilihind}')
+        scr1 = scr1.query("kode in @subind and p>=@awal and p<=@akhir and om>=@omawal and om<=@omakhir and dev>=@devawal and dev<=@devakhir and roe>=@roeawal and roe<=@roeakhir and tun>=@tunawal and tun<=@tunakhir and pbvy>=@nbawal and pbvy<=@nbakhir")
     else:
        st.subheader('Screener Saham Dengan Fraksi Harga Lebih Dari 5000')
        scr1 = scr1.query("skg > 5000 and p>=@awal and p<=@akhir and om>=@omawal and om<=@omakhir and dev>=@devawal and dev<=@devakhir and roe>=@roeawal and roe<=@roeakhir and tun>=@tunawal and tun<=@tunakhir and pbvy>=@nbawal and pbvy<=@nbakhir")
