a0d8df665ab425295cbbcbb3020e9a96091ffa99
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-04-27 17:39:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -716,7 +716,7 @@ def screener():
     col1, col2, col3, col4, col5, col6 = st.columns([1,1,1,1,1,1], gap="large")
     with col1:
         awal, akhir = st.slider('Posisi', min_value=0, max_value=100, value=(0, 100))
-        scr2.query("p>=@awal and p<=@akhir)   
+        scr2.query("p>=@awal and p<=@akhir")   
     with col2:
         omawal, omakhir = st.slider('Margin Operasi (%)', min_value=0, max_value=100, value=(0, 100))
         omawal = omawal/100
