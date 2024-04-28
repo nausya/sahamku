416d0ff052a1d88132673a019de608bdefe42a7d
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-04-28 14:18:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -474,10 +474,8 @@ else:
    bmd = pd.read_csv('Finansial.csv', sep=';', usecols=['Kode','KodeInd','EPSRP','BVRP','PER','PBV','DER','ROA(%)','ROE(%)','NPM(%)']).sort_values('Kode')
    bmd = bmd.query("KodeInd == @fin[5]")
    bmd = bmd[['Kode','EPSRP','BVRP','PER','PBV','DER','ROA(%)','ROE(%)','NPM(%)']]
-   #bmd = bmd.set_index('Kode')
-   bmd = bmd.style.apply(lambda row: ['background-color: pink' if row['Kode'] == '@kodesaja' else '' for code in row], axis=1)
+   bmd = bmd.set_index('Kode')
    bmd
-    #st.dataframe(bmd) 
    st.subheader("", divider="rainbow")
    ################## END OF BENCHMARK DETIL
 
