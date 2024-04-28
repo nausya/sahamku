8ec5ad8f5a51b8d097019165b592ae5c22f8cd1a
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-04-28 13:48:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -475,6 +475,7 @@ else:
    bmd = bmd.query("KodeInd == @fin[5]")
    bmd = bmd[['Kode','EPSRP','BVRP','PER','PBV','DER','ROA(%)','ROE(%)','NPM(%)']]
    bmd = bmd.set_index('Kode')
+   bmd = bmd.style.applymap(lambda x: 'background-color: pink' if x in 'Kode' else '')
    st.dataframe(bmd) 
    st.subheader("", divider="rainbow")
    ################## END OF BENCHMARK DETIL
