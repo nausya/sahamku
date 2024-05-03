ceb912d9c35af03ca8f5e89587299e95a24105db
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-05-03 22:49:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -800,7 +800,7 @@ def screener():
     st.write('Filter Berdasarkan :')
     col1, col2, col3, col4, col5, col6 = st.columns([1,1,1,1,1,1], gap="large")
     with col1:
-        awal, akhir = st.slider('Posisi', min_value=minp, max_value=maxp, value=(minp, maxp))   
+        awal, akhir = st.slider('Posisi', min_value=0, max_value=100, value=(0, 100))   
     with col2:
         omawal, omakhir = st.slider('Margin Operasi', min_value=minom, max_value=maxom, value=(minom, maxom))
         omawal = omawal
