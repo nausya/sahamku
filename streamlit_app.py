d9ecb300aa1891a9a585e006099005166fca7db2
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-05-04 17:42:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -852,5 +852,5 @@ def screener():
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 if __name__ == '__main__':
     main()
-#st.experimental_rerun()  
+#st.rerun()  
 #st.sidebar.info("@2024")
