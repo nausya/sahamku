43a37634d2bde2f33ea918421a6146901b10ddd7
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-05-09 22:55:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -617,7 +617,7 @@ def tech_indicators():
     fg = fg.query("Kode == @kodesaja")
     #fg = fg[["Kode","Tanggal","Proft","ProftOwner"]]
     st.write('Penyandingan Fundamental')
-    st.line_chart(fg, x="Tanggal", y=["Proft","ProftOwner"])
+    st.line_chart(fg, x="Tanggal", y=["Aset","Utang","Modal","Penjual","EBT(RP)","Proft","ProftOwner","EPS(RP)","BV(RP)","PER","PBV","DER","ROA(%)","ROE(%)","NPM(%)"])
     ########### end of Grafik Funda
 #################### CARI DATA ###############
 def dataframe():
