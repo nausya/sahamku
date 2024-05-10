8679073985a26eaa98926278e13bff5083c0413b
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-05-10 22:15:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -566,12 +566,7 @@ st.info('Untuk jangka panjang perlu diperhatikan kisaran posisi harga kurang dar
 
 def tech_indicators():
     st.header('Teknikal Indikator')
-    #option = st.radio('Pilih Teknikal Indikator', ['Close', 'BB', 'MACD', 'RSI', 'EMA'])
-    options = ['Close', 'BB', 'MACD', 'RSI', 'EMA']
-    kols = st.columns(5)
-    for kol, option in zip(kols, options):
-        # Menampilkan radio button di setiap kolom
-        selected_option = kol.radio("Pilih Teknikal Indikator", options)
+    option = st.radio('Pilih Teknikal Indikator', ['Close', 'BB', 'MACD', 'RSI', 'EMA'])
         
     # Bollinger bands
     bb_indicator = BollingerBands(data.Close)
@@ -588,7 +583,7 @@ def tech_indicators():
     #sma = SMAIndicator(data.Close, window=14).sma_indicator()
     # EMA
     ema = EMAIndicator(data.Close).ema_indicator()
-    option = selected_option
+
     if option == 'Close':
         st.write('Close Price')
         st.line_chart(data.Close)
